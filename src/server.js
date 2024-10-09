6d84c15c5dcb86ca96646831028f01e0a97a83b8
OksanaGukova <kseniagukova@ukr.net>
2024-10-09 13:49:00 +0300

    start

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+
+export const startServer = () => {
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+const PORT = Number(env('PORT', 3000));
+
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: '',
+  });
+});
+
+app.use('*', (req, res, next) => {
+  res.status(400).json({
+    message: 'Not Found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+}
