15630d678c06cb552769679331affb7c711e8bec
OksanaGukova <kseniagukova@ukr.net>
2024-10-09 13:58:01 +0300

    update

--- src/server.js
+++ src/server.js
@@ -4,43 +4,43 @@ import cors from 'cors';
 import { env } from './utils/env.js';
 
 export const startServer = () => {
-const app = express();
+  const app = express();
 
-app.use(express.json());
-app.use(cors());
+  app.use(express.json());
+  app.use(cors());
 
 
-const PORT = Number(env('PORT', 3000));
+  const PORT = Number(env('PORT', 3000));
 
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-app.get('/', (req, res) => {
-  res.json({
-    message: '',
+  app.get('/', (req, res) => {
+    res.json({
+      message: '',
+    });
   });
-});
 
-app.use('*', (req, res, next) => {
-  res.status(400).json({
-    message: 'Not Found',
+  app.use('*', (req, res, next) => {
+    res.status(400).json({
+      message: 'Not Found',
+    });
   });
-});
 
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
   });
-});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 
-}
+};
